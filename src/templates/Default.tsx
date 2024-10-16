4fdc837a7404011bcb33272f1f768706831bd4ce
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-16 10:44:30 +0000

    feat: adding initial structure

---
+++ src/templates/Default.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { Container } from '@mui/material';
+
+interface DefaultProps {
+    children: React.ReactNode;
+}
+
+const Default = ({children}: DefaultProps) => {
+
+    return (
+        <>
+            <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: 10}}>
+                <Header />
+            </div>
+            <Container
+                maxWidth='lg'
+            >
+                {children}
+
+            </Container>
+            <Footer />
+        </>
+    )
+}
+
+export default Default
