692ad775bea3d76f307743954bb41ac2b047ae94
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-16 13:02:40 +0000

    feat: adding facebook and instagram icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,8 @@ const StyledHeader = styled.header`
     align-items: center;
 `;
 
-const StyledButton = styled.button`
+const StyledButton = styled.a`
+    display: inline-block;
     width: 88px;
     margin-right: 2px;
     padding: 7px;
@@ -24,6 +25,7 @@ const StyledButton = styled.button`
     transition: all 500ms ease-in-out;
     background-color: rgba(131, 130, 130, 0.2);
     border: 1px solid transparent;
+    text-align: center;
 
     &:hover {
         color: black;
@@ -31,54 +33,79 @@ const StyledButton = styled.button`
         filter: brightness(0.3);
         border: 1px solid #4b484845;
     }
-`
+`;
 
 const StyledNav = styled.nav`
     padding: 3px 3px 3px;
     border: 1px solid gray;
     border-radius: 20px / 70px;
-`
+`;
 
+const Div = styled.div`
+    display: flex;
+    position: absolute;
+    top: 15%;
+    right: 10px;
+`
+const StyledIcon = styled.a`
+    transition: transform 200ms ease-in-out;
+    margin-right: 5px;
+    &:hover {
+        transform: scale(1.1); /* Efeito de escala no hover */
+    }
+`;
 
 const Header = () => {
     return (
         <StyledHeader>
-
             <Image  
                 src="/images/tesoura-pentes-icon.webp"  // Corrigido caminho
                 alt="Developer"
                 width={100}
                 height={60}
-                style={{ position: 'absolute', top: 0}}
+                style={{ position: 'absolute', top: 0 }}
             />
-            <Container
-                maxWidth='sm'
-                sx={{}}
-            >
+            <Container maxWidth='sm'>
                 <StyledNav>
-
-                    <StyledButton style={{marginLeft: '4px'}}>
+                    <StyledButton style={{ marginLeft: '4px' }} >
                         Home
                     </StyledButton>
-                    <StyledButton>
+                    <StyledButton >
                         Gallery
                     </StyledButton>
-                    <StyledButton>
+                    <StyledButton >
                         Team
                     </StyledButton>
-                    <StyledButton>
+                    <StyledButton >
                         Services
                     </StyledButton>
-                    <StyledButton>
+                    <StyledButton >
                         News
                     </StyledButton>
-                    <StyledButton>
+                    <StyledButton >
                         Career
                     </StyledButton>
                 </StyledNav>
             </Container>
+            <Div>
 
-            
+            <StyledIcon href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                <Image  
+                    src="/images/facebook.svg"  // Corrigido caminho
+                    alt="Facebook"
+                    width={40}
+                    height={40}
+                />
+            </StyledIcon>
+            <StyledIcon href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                <Image  
+                    src="/images/instagram-fill.svg"  // Corrigido caminho
+                    alt="Facebook"
+                    width={40}
+                    height={40}
+                />
+            </StyledIcon>
+            </Div>
         </StyledHeader>
     );
 }
