3b7ca5c0c7d6679889beff1fe2884e27ee691d4f
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-18 12:52:11 +0000

    feat: adding responsivity to gallery page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -5,48 +5,50 @@ import 'react-multi-carousel/lib/styles.css';
 import Image from "next/image";
 
 const StyledSection = styled.section`
-
     width: 100%;
     height: 100vh;
     padding-top: 45px;
     display: flex;
     align-items: center;
 `
+
 const StyledCarousel = styled(Carousel)`
     border-radius: 15px;
 
+    @media (max-width: 376px) {
+        display: flex;
+        margin-left: 15px;
+        padding: 10px;
+        & .image {
+            border-radius: 15px;  /* Borda arredondada nas imagens */
+        }
+    }
 `
 
 const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
+        breakpoint: { max: 464, min: 0 },
+        items: 1
     }
 };
 
-
 const Gallery = () => {
-
     return (
         <>
             <StyledSection>
-
-                <Container
-                    maxWidth='md'
-                >   
+                <Container maxWidth='md'>   
                     <Typography 
                         color="white"
                         variant="h4"
@@ -67,34 +69,39 @@ const Gallery = () => {
                         rewindWithAnimation={true}
                     >
                         <Image  
-                            src="/images/barber-shop-1.png"  // Corrigido caminho
-                            alt="Developer"
+                            src="/images/barber-shop-1.png"  
+                            alt="Barber Shop 1"
                             width={300}
-                            height={300}                            
+                            height={300}
+                            className="image"  // Classe adicionada
                         />
                         <Image  
-                            src="/images/barber-shop-2.png"  // Corrigido caminho
-                            alt="Developer"
+                            src="/images/barber-shop-2.png"  
+                            alt="Barber Shop 2"
                             width={300}
-                            height={300}                            
+                            height={300}
+                            className="image"  // Classe adicionada
                         />
                         <Image  
-                            src="/images/barber-shop-3.png"  // Corrigido caminho
-                            alt="Developer"
+                            src="/images/barber-shop-3.png"  
+                            alt="Barber Shop 3"
                             width={300}
-                            height={300}                            
+                            height={300}
+                            className="image"  // Classe adicionada
                         />
                         <Image  
-                            src="/images/barber-shop-4.png"  // Corrigido caminho
-                            alt="Developer"
+                            src="/images/barber-shop-4.png"  
+                            alt="Barber Shop 4"
                             width={300}
-                            height={300}                            
+                            height={300}
+                            className="image"  // Classe adicionada
                         />
                         <Image  
-                            src="/images/barber-shop-5.png"  // Corrigido caminho
-                            alt="Developer"
+                            src="/images/barber-shop-5.png"  
+                            alt="Barber Shop 5"
                             width={300}
-                            height={300}                            
+                            height={300}
+                            className="image"  // Classe adicionada
                         />
                     </StyledCarousel>
                 </Container>
